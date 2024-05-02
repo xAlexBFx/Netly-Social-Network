b4a7e24afc377ff28a9cd22296e9e8b1491a2371
Alex Betances <alexbetancesf@gmail.com>
2024-05-01 22:30:46 -0400

    ...

--- Netly/App.js
+++ Netly/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import ProfileScreen from './screens/ProfileScreen.js';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <ProfileScreen/>
+    </NavigationContainer>
   );
 }
 
